e8e2a4e51f2411ca0ad2a45e5de11c22b4e153f5
amir ukbazgi <amirukbazgi@gmail.com>
2026-08-30 22:45:53 +0200

    creating collection page

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -1,5 +1,189 @@
+import { useEffect, useState } from "react";
+import { useShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+import ProductItem from "../components/ProductItem";
+
 function Collection() {
-  return <div>This is a collection page</div>;
+  const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
+  const [sortType, setSortType] = useState("relevant");
+  const { products } = useShopContext();
+
+  const toggleCategory = (e) => {
+    if (categories.includes(e.target.value)) {
+      setCategories((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategories((prev) => [...prev, e.target.value]);
+    }
+  };
+  const toggleSubCategory = (e) => {
+    if (subCategories.includes(e.target.value)) {
+      setSubCategories((prev) =>
+        prev.filter((item) => item !== e.target.value),
+      );
+    } else {
+      setSubCategories((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const applyFilter = () => {
+    let productsCopy = products.slice();
+
+    if (categories.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        categories.includes(item.category),
+      );
+    }
+    if (subCategories.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategories.includes(item.subCategory),
+      );
+    }
+    setFilterProducts(productsCopy);
+  };
+
+  const sortProduct = () => {
+    let filterProductCopy = filterProducts.slice();
+    switch (sortType) {
+      case "low-high":
+        setFilterProducts(filterProductCopy.sort((a, b) => a.price - b.price));
+        break;
+      case "high-low":
+        setFilterProducts(filterProductCopy.sort((a, b) => b.price - a.price));
+        break;
+      default:
+        applyFilter();
+        break;
+    }
+  };
+
+  useEffect(() => {
+    applyFilter();
+  }, [categories, subCategories]);
+
+  useEffect(() => {
+    sortProduct();
+  }, [sortType]);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter Options */}
+      <div className="min-w-60">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center gap-2 cursor-pointer"
+        >
+          Filters
+          <img
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+            src={assets.dropdown_icon}
+            alt="dropdown image"
+          />
+        </p>
+        {/* Category Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? "" : "hidden"} sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium uppercase">Categories</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input
+                type="checkbox"
+                value={"Men"}
+                onChange={toggleCategory}
+                className="w-3"
+              />{" "}
+              Men
+            </p>
+            <p className="flex gap-2">
+              <input
+                type="checkbox"
+                value={"Women"}
+                onChange={toggleCategory}
+                className="w-3"
+              />{" "}
+              Women
+            </p>
+            <p className="flex gap-2">
+              <input
+                type="checkbox"
+                value={"Kids"}
+                onChange={toggleCategory}
+                className="w-3"
+              />{" "}
+              Kids
+            </p>
+          </div>
+        </div>
+        {/* Type Subcategory Filter */}
+        <div
+          className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? "" : "hidden"} sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium uppercase">Type</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input
+                type="checkbox"
+                value={"Topwear"}
+                onChange={toggleSubCategory}
+                className="w-3"
+              />{" "}
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input
+                type="checkbox"
+                value={"Bottomwear"}
+                onChange={toggleSubCategory}
+                className="w-3"
+              />{" "}
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input
+                type="checkbox"
+                value={"Winterwear"}
+                onChange={toggleSubCategory}
+                className="w-3"
+              />{" "}
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Side  */}
+      <div className="flex-1">
+        <div className="flex items-center justify-between text-base sm:text-2xl mb-4">
+          <Title text1={"ALL"} text2={"COLLECTIONS"} />
+          {/* Product sort */}
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border-2 border-gray-300 text-sm px-2"
+          >
+            <option value="relevant">Sort by: Relevant</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
+        </div>
+        {/* Map Products */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  gap-4 gap-y-6">
+          {filterProducts?.map((product) => (
+            <ProductItem
+              key={product._id}
+              id={product._id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Collection;
